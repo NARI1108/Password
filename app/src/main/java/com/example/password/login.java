1804a9ec2290d64690d53bbd187a1e4e9821690e
Hill <125409221+NARI1108@users.noreply.github.com>
2023-12-29 03:08:28 +0330

    We created a base activity to prevent code duplication, when the number of duplicate codes in our app is less, the performance of our app will be higher. In this basic activity, we have first linked the views of all XML layers, and then other activities will use these views by inheritance.

--- app/src/main/java/com/example/password/login.java
+++ app/src/main/java/com/example/password/login.java
@@ -1,7 +1,5 @@
 package com.example.password;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,9 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class login extends AppCompatActivity {
-    EditText edt_mobile, edt_password;
-    Button btn_save;
+public class login extends base {
     SharedPreferences SharedP;
     public static final String LoginInfo = "login_info";
     public static final String Mobile_NO = "mobile_no";
@@ -25,12 +21,6 @@ public class login extends AppCompatActivity {
         findViews();
         saved();
     }
-//    This method actually connects views and layers from an XML layer to this layer.
-    public void findViews(){
-        edt_password = findViewById(R.id.edt_password);
-        edt_mobile = findViewById(R.id.edt_mobile);
-        btn_save = findViewById(R.id.btn_save);
-     }
 //     This method is for saving user information.
      public void saved(){
         SharedP = getSharedPreferences(LoginInfo, Context.MODE_PRIVATE);
