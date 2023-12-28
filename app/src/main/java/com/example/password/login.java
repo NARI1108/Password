9ba248a6b4cc7277d2272b72d174fd1bf15751ae
Hill <125409221+NARI1108@users.noreply.github.com>
2023-12-28 23:55:35 +0330

    We created a method to store user information such as: phone number and password

--- app/src/main/java/com/example/password/login.java
+++ app/src/main/java/com/example/password/login.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
     EditText edt_mobile, edt_password;
     Button btn_save;
+    SharedPreferences SharedP;
+    public static final String LoginInfo = "login_info";
+    public static final String Mobile_NO = "mobile_no";
+    public static final String Password  = "password";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findViews();
-
+        saved();
     }
 //    This method actually connects views and layers from an XML layer to this layer.
     public void findViews(){
@@ -24,4 +31,22 @@ public class login extends AppCompatActivity {
         edt_mobile = findViewById(R.id.edt_mobile);
         btn_save = findViewById(R.id.btn_save);
      }
+//     This method is for saving user information.
+     public void saved(){
+        SharedP = getSharedPreferences(LoginInfo, Context.MODE_PRIVATE);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//              It takes the text or password and stores it inside the variable (stores information in a layer).
+                String phone = edt_mobile.getText().toString();
+                String password = edt_password.getText().toString();
+                SharedPreferences.Editor mobile_edit = SharedP.edit();
+                mobile_edit.putString(Mobile_NO,phone);
+                mobile_edit.putString(Password,password);
+                mobile_edit.apply();
+                Toast.makeText(login.this, getResources().getString(R.string.saving), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+        }
 }
