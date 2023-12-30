83f8fa214a855591a9b10c661e790f09372411f9
Hill <125409221+NARI1108@users.noreply.github.com>
2023-12-30 22:37:07 +0330

    The function of this method is that if the user has not registered his information, he can register his information, and if his information is registered, the other person does not need to register his information.

--- app/src/main/java/com/example/password/MainActivity.java
+++ app/src/main/java/com/example/password/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.password;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends base {
-
+    boolean firstTime=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        saved();
+
+    }
+//    This method is the first time the app is run when the user's information is registered in the app.
+    public void saved(){
+        SharedPreferences.Editor runEdit = SharedP.edit();
+        firstTime = SharedP.getBoolean("First",true);
+//        This method specifies that the main activity should be executed once.
+        if(firstTime){
+            startActivity(new Intent(MainActivity.this,login.class));
+            runEdit.putBoolean("first",false);
+            runEdit.apply();
+        }
+        if (SharedP.contains(Password)){
+            runEdit.putBoolean("first",false);
+            runEdit.apply();
+        }
     }
 
 }
