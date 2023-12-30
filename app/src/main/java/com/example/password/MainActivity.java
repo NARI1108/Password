018105ea8f890f5bd6fa1b33369953c189444fc6
Hill <125409221+NARI1108@users.noreply.github.com>
2023-12-30 22:38:53 +0330

    In this method, you can use all the options you added to the app, such as forgetting the password and changing the password, sending messages to the user, etc.

--- app/src/main/java/com/example/password/MainActivity.java
+++ app/src/main/java/com/example/password/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends base {
         setContentView(R.layout.activity_main);
         findViews();
         saved();
-
+        setOnClick();
     }
 //    This method is the first time the app is run when the user's information is registered in the app.
     public void saved(){
@@ -32,5 +32,49 @@ public class MainActivity extends base {
             runEdit.apply();
         }
     }
-
+//    This method is for applying clicking on activity options.
+    public void setOnClick(){
+//        This pseudo code is used when the user has forgotten his password.
+        txt_forgotten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String PASSWORD = SharedP.getString("Password",getString(R.string.MESSAGE));
+                String MOBILE = SharedP.getString("Mobile_No",getString(R.string.MESSAGE));
+//                This pseudocode is for sending messages to the user if he has registered his information.
+                try {
+                    SmsManager sms = SmsManager.getDefault();
+                    sms.sendTextMessage(MOBILE,null,PasswordMessage+PASSWORD,null,null);
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.sms), Toast.LENGTH_LONG).show();
+                }catch(Exception e){
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.message), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//        This code is for changing the password.
+        txt_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String PASSWORD = SharedP.getString("Password",getString(R.string.MESSAGE));
+                String Password_Entry = edt_password.getText().toString();
+                if (Password_Entry.equals(PASSWORD)){
+                    startActivity(new Intent(MainActivity.this,login.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "AtFirst Enter your Previous Password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+//        This pseudo-code is for going from this main activity to the next activity (home).
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             String PASSWORD = SharedP.getString("Password",getString(R.string.MESSAGE));
+             String Password_Entry = edt_password.getText().toString();
+             if (Password_Entry.equals(PASSWORD)){
+                 startActivity(new Intent(MainActivity.this,Home.class));
+             }else {
+                 Toast.makeText(MainActivity.this, "your password is wrong!", Toast.LENGTH_LONG).show();
+             }
+            }
+        });
+    }
 }
